3864990e49a92f97f462cae56e79e003cd9cc873
Alexis <alexispolo@github.com>
2025-04-22 13:03:58 -0300

    feat: :sparkles: Se crearon graficos

---
+++ graficos/heatmap.py
@@ -0,0 +1,24 @@
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
+# heatmap_data = df.groupby('Genre')[['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']].mean()
+
+# # Ajustar tamaño del gráfico
+# plt.figure(figsize=(12, 8))
+
+# # Crear heatmap
+# sns.heatmap(
+#     heatmap_data, 
+#     annot=True,  # Muestra valores en las celdas
+#     fmt=".2f",   # Formato: 2 decimales
+#     cmap="YlOrRd",  # Paleta de colores (amarillo-naranja-rojo)
+#     linewidths=0.5  # Líneas divisorias
+# )
+
+# # Personalizar
+# plt.title('Ventas Promedio por Género y Región (en millones)', fontsize=14)
+# plt.xlabel('Región', fontsize=12)
+# plt.ylabel('Género', fontsize=12)
+# plt.xticks(rotation=45)  # Rotar etiquetas del eje X
+
+# plt.show()
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
+
 df = pd.read_csv('./data/vgsales.csv')
 
 print(df.head())
+
+
+# # para guardar en json
+# import json
+
+#* esto para guardar en json
+# Nombre_Grafico.to_json('nombre_grafico.json', orient='records')
+
+#* esto para react en un componente
+# import nombrecomponente from './nombrecomponente.json'
